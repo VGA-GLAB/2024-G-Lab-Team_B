544ea07790c45838f3b1041aa3215dffef58b34b
Shigema <tsubakyun4869@gmail.com>
2024-02-23 17:02:56 +0900

    [add] プレハブ・スクリプト・NPCのフォルダ NPCプレハブ、スクリプト（２つ）、NPCにつけたマテリアル

---
+++ Assets/Scripts/NPC/NPCStateMachine.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// NPCのステートマシン
+/// </summary>
+public class NPCStateMachine
+{
+    StateBase _currentState;
+
+    public void ChangeState(StateBase newState)
+    {
+        _currentState?.Exit();
+        _currentState = newState;
+        _currentState.Enter();
+    }
+
+    public void Update()
+    {
+        _currentState?.Update();
+    }
+}
+
+/// <summary>
+/// ステートの基底クラス
+/// </summary>
+public abstract class StateBase
+{
+    [Tooltip("このステートマシンを使用するインスタンスを保持")] protected NPC _npc;
+
+    public StateBase(NPC npc)
+    {
+        this._npc = npc;
+    }
+
+    public abstract void Enter();
+    public abstract void Update();
+    public abstract void Exit();
+}
