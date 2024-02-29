88bb4709176c6d12570e724d970ec70ac1fb58ee
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-02-29 16:59:25 +0900

    [update]プレイヤーの視界の更新 ・過去のプレイヤーの視界が視錘台になるよう変更 ・過去のプレイヤーの視界判定が壁にさえぎられるように更新

--- Assets/Scripts/PlayerRecord/RecordedPlayerSight.cs
+++ Assets/Scripts/PlayerRecord/RecordedPlayerSight.cs
@@ -3,16 +3,17 @@ using UnityEngine.UI;
 
 public class RecordedPlayerSight : MonoBehaviour
 {
-    [SerializeField, Header("視点方向を示すオブジェクト")]
-    private Transform lookAtTarget;
+    [SerializeField, Header("視界判定用のカメラ")]
+    private Camera _sightCamera;
 
-    [SerializeField, Header("視野角"), Range(0, 180)]
-    private float sightAngle;
+    [SerializeField, Header("見える距離")]
+    private float _sightDistance = 10f;
 
-    [SerializeField, Header("見える距離")] private float _sightDistance = 5f;
+    [SerializeField, Header("視界判定用のRayCastが当たるレイヤー")]
+    private LayerMask _targetLayerMask;
 
     private Text _visibleMessage; // 発見テキスト
-    private Transform _target; // 発見したいオブジェクト
+    private GameObject _player; // 発見したいオブジェクト
     private bool _isVisible = true; // 発見フラグ
 
     private void Start()
@@ -30,36 +31,44 @@ public class RecordedPlayerSight : MonoBehaviour
         } // 論理積（最後のフレームと現フレームで見える/見えないが切り替わった時）
     }
 
-    /// <summary>発見フラグの計算</summary>
+    /// <summary>プレイヤー発見の判定を行います</summary>
     private bool IsVisible()
     {
-        // 発見したいオブジェクトがない場合はプレイヤーを探して割り当てる
-        if (!_target)
+        // プレイヤーを探して割り当てる
+        if (!_player)
         {
-            var player = GameObject.FindGameObjectWithTag("Player");
-
-            if (player)
-                _target = player.transform;
+            _player = GameObject.FindGameObjectWithTag("Player");
         }
 
-        Vector3 look = lookAtTarget.position - this.transform.position; // 視点方向ベクトル
-        Vector3 target = _target.position - this.transform.position; // ターゲットへのベクトル
-        float cosHalfSight = Mathf.Cos(sightAngle / 2 * Mathf.Deg2Rad); // 視野角（の半分）の余弦
-        float cosTarget = Vector3.Dot(look, target) / (look.magnitude * target.magnitude); // ターゲットへの角度の余弦
-        return cosTarget > cosHalfSight &&
-               target.magnitude < _sightDistance; // ターゲットへの角度が視界の角度より小さく、かつ距離が視界より近いなら見えていると判定して true を返す
-    }
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_sightCamera);
 
-    private void OnDrawGizmos()
-    {
-        // 視界の範囲（正面及び左右の端）をギズモとして描く
-        Vector3 lookAtDirection = (lookAtTarget.position - this.transform.position).normalized; // 正面（正規化）
-        Vector3 rightBorder = Quaternion.Euler(0, sightAngle / 2, 0) * lookAtDirection; // 右端（正規化）
-        Vector3 leftBorder = Quaternion.Euler(0, -1 * sightAngle / 2, 0) * lookAtDirection; // 左端（正規化）
-        Gizmos.color = Color.cyan; // 正面は水色で描く
-        Gizmos.DrawRay(this.transform.position, lookAtDirection * _sightDistance);
-        Gizmos.color = Color.blue; // 両端は青で描く
-        Gizmos.DrawRay(this.transform.position, rightBorder * _sightDistance);
-        Gizmos.DrawRay(this.transform.position, leftBorder * _sightDistance);
+        if (GeometryUtility.TestPlanesAABB(planes, _player.GetComponent<Collider>().bounds))
+        {
+            Vector3 dir = _player.transform.position - transform.position;
+            Ray ray = new Ray(this.transform.position, dir.normalized);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, _sightDistance, _targetLayerMask))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+#if UNITY_EDITOR
+                    Debug.DrawRay(this.transform.position, hit.point - this.transform.position, Color.green);
+#endif
+                    
+                    return true;
+                }
+                else
+                {
+#if UNITY_EDITOR
+                    Debug.DrawRay(this.transform.position, hit.point - this.transform.position, Color.red);
+#endif
+                    
+                    return false;
+                }
+            }
+        }
+
+        return false;
     }
 }
