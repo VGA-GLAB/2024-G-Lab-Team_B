10d4644d4332e848587d8ef63c778315608fca58
RK <riku.kuu0421@gmail.com>
2024-03-22 11:18:32 +0900

    [add]PlayerのHPが減った時白く濁るようにした

---
+++ Assets/Scripts/PlayerRecord/PlayerDamagePresenter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class PlayerDamagePresenter : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerHPController _playerHP;
+
+    [SerializeField]
+    private PlayerDamageView _damageView;
+
+    private void Start()
+    {
+        _playerHP.HitPoint.Skip(1).Subscribe(damage => _damageView.UpdatePanelAlpha(_playerHP.MaxHitPoint - damage)).AddTo(this);
+    }
+}
