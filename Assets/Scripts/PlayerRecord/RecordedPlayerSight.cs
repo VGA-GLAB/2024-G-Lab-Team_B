ae3feb3c642ffd3a8bf26c3adeddb65511353d9c
RK <riku.kuu0421@gmail.com>
2024-03-23 17:52:13 +0900

    [update]HPを回復するようにした

--- Assets/Scripts/PlayerRecord/RecordedPlayerSight.cs
+++ Assets/Scripts/PlayerRecord/RecordedPlayerSight.cs
@@ -16,13 +16,21 @@ public class RecordedPlayerSight : MonoBehaviour
     [SerializeField, Header("発見時のダメージ数")]
     private int _damageDealt = 20;
 
+    [SerializeField, Header("自然回復する値")]
+    private int _healHP = 1;
+
+    [SerializeField, Header("自然回復のインターバル")]
+    private float _healInterval = 1f;
+
     [SerializeField, Header("ダメージのインターバル")]
     private float _damageInterval = 1f;
 
     private Text _visibleMessage; // 発見テキスト
     private GameObject _player; // 発見したいオブジェクト
+    private IDamage _iDamage;
     private bool _isVisible = true; // 発見フラグ
     private float _visiblTime = 0f; // 見つけている時間
+    private float _healTime = 0f;
     private bool _isDamage = false;
 
     private void Start()
@@ -31,6 +39,8 @@ public class RecordedPlayerSight : MonoBehaviour
         //TODO:複数人分の判定の表示に対応しないといけない
         // 発見テキストの検索
         _visibleMessage = FindFirstObjectByType<Text>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _iDamage = _player.GetComponent<IDamage>();
     }
 
     private void Update()
@@ -43,19 +53,19 @@ public class RecordedPlayerSight : MonoBehaviour
     /// </summary>
     private void FindPlyer()
     {
-        if (IsVisible() && _player.TryGetComponent<IDamage>(out IDamage damage))
+        if (IsVisible())
         {
             _visiblTime += Time.deltaTime;
 
             if (!_isDamage)//初回ダメージ
             {
-                damage.SendDamage(_damageDealt);
+                _iDamage.SendDamage(_damageDealt);
                 _isDamage = true;
             }
 
             if (_visiblTime >= _damageInterval)
             {
-                damage.SendDamage(_damageDealt);
+                _iDamage.SendDamage(_damageDealt);
                 _visiblTime = 0f;
             }
             //_isVisible = !_isVisible;
@@ -63,6 +73,12 @@ public class RecordedPlayerSight : MonoBehaviour
         } // 論理積（最後のフレームと現フレームで見える/見えないが切り替わった時）
         else
         {
+            _healTime += Time.deltaTime;
+            if (_healTime >= _healInterval)
+            {
+                _iDamage.SendDamage(-_healHP);
+                _healTime = 0;
+            }
             _isDamage = false;
             _visiblTime = 0f;
         }
@@ -71,12 +87,6 @@ public class RecordedPlayerSight : MonoBehaviour
     /// <summary>プレイヤー発見の判定を行います</summary>
     private bool IsVisible()
     {
-        // プレイヤーを探して割り当てる
-        if (!_player)
-        {
-            _player = GameObject.FindGameObjectWithTag("Player");
-        }
-
         // カメラから視錘台の形の範囲を取得します
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_sightCamera);
 
