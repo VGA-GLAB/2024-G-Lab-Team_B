dfb6822c2cc542f350aecd6a9b3d1eec4c063322
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-03-28 18:02:14 +0900

    [update]名誉教授のStartStateの処理を実装

--- Assets/Scripts/Event/Professor/State/StartState.cs
+++ Assets/Scripts/Event/Professor/State/StartState.cs
@@ -1,4 +1,5 @@
 using GameAutomaton;
+using UnityEngine;
 
 namespace GameEvent
 {
@@ -7,19 +8,25 @@ namespace GameEvent
         // 日本語対応
         public class StartState : StateMachine<ProfessorController>.StateBase
         {
+            private float _transitionTime = 30f; // 遷移する時間
+            private float _timer; // タイマー
+
             public override void OnEnter()
             {
-                
             }
 
             public override void OnUpdate()
             {
-                
+                if (_timer > _transitionTime)
+                {
+                    StateMachine.Transition((int)ProfessorAction.TakeStroll);
+                }
+
+                _timer += Time.deltaTime;
             }
 
             public override void OnExit()
             {
-                
             }
         }
     }
