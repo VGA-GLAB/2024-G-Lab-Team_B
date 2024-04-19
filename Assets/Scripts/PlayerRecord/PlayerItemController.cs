6d118d9b6f33fd13248b0ab46c0453b503e87c35
RK <riku.kuu0421@gmail.com>
2024-04-20 00:18:50 +0900

    [update]アイテムを設置できるようにした

--- Assets/Scripts/PlayerRecord/PlayerItemController.cs
+++ Assets/Scripts/PlayerRecord/PlayerItemController.cs
@@ -9,7 +9,11 @@ public class PlayerItemController : MonoBehaviour
 
     private List<ItemBase> _inventory = new List<ItemBase>(); // プレイヤーのインベントリ
 
-    private ReactiveProperty<int> _selectItemIndex = new(); // 選択されたアイテム
+    private ReactiveProperty<int> _selectItemIndex = new(-1); // 選択されたアイテム
+
+    private Subject<Unit> _drop = new();
+
+    public ISubject<Unit> Drop => _drop;
 
     public IReactiveProperty<int> SelectItemIndex => _selectItemIndex;
 
@@ -17,19 +21,17 @@ public class PlayerItemController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, _maxDistance))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, _maxDistance))
+            if (Input.GetMouseButtonDown(0))
             {
-                GetItem(hit);
-                if (_inventory?.Count > 0)
+                if (_inventory?.Count > 0 && !SelectUseItem(hit) && !hit.collider.TryGetComponent<ItemBase>(out ItemBase item))
                 {
-                    UseDrug(hit);
-                    UseAED(hit);
+                    DropItem(hit);
                 }
+                GetItem(hit);
             }
         }
         ChangeSelectedItem();
@@ -44,7 +46,11 @@ public class PlayerItemController : MonoBehaviour
         if (hit.collider.TryGetComponent<ItemBase>(out ItemBase item))
         {
             AddItem(item);
-            hit.collider.gameObject.SetActive(false);
+            item.gameObject.SetActive(false);
+            if(_inventory.Count == 1)
+            {
+                _selectItemIndex.Value = 0;
+            }
         }
     }
 
@@ -59,47 +65,57 @@ public class PlayerItemController : MonoBehaviour
     /// <summary>
     /// 選択されたアイテムを変更
     /// </summary>
-    void ChangeSelectedItem()
+    private void ChangeSelectedItem()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            _selectItemIndex.Value++;
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            _selectItemIndex.Value--;
-        }
-
-        if (_selectItemIndex.Value >= _inventory.Count)
+        if (Input.GetKeyDown(KeyCode.Alpha1) && 1 <= _inventory.Count)
         {
             _selectItemIndex.Value = 0;
         }
-        else if (_selectItemIndex.Value < 0)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && 2 <= _inventory.Count)
         {
-            _selectItemIndex.Value = _inventory.Count - 1;
+            _selectItemIndex.Value = 1;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3) && 3 <= _inventory.Count)
+        {
+            _selectItemIndex.Value = 2;
         }
     }
 
     /// <summary>
-    /// 薬を使用する
+    /// 選択したアイテムを設置してインベントリから削除
     /// </summary>
-    private void UseDrug(RaycastHit hit)
+    private void DropItem(RaycastHit hit)
+    {
+        _inventory[_selectItemIndex.Value].transform.position = hit.point;
+        _inventory[_selectItemIndex.Value].gameObject.SetActive(true);
+        _inventory.RemoveAt(_selectItemIndex.Value);//インベントリから削除
+
+        if (_selectItemIndex.Value >= _inventory.Count)
+        {
+            _selectItemIndex.Value = _inventory.Count - 1;
+        }
+        else
+        {
+            _drop.OnNext(Unit.Default);//ドロップ時に通知する
+        }
+    }
+
+    /// <summary>
+    /// 選択したアイテムを使用
+    /// </summary>
+    private bool SelectUseItem(RaycastHit hit)
     {
         if (hit.collider.tag == "Poison" && _inventory[_selectItemIndex.Value].ItemType == ItemType.Drug)
         {
             _inventory[_selectItemIndex.Value].UseItem(hit.collider.gameObject);
+            return true;
         }
-    }
-
-    /// <summary>
-    /// AEDを使用する
-    /// </summary>
-    private void UseAED(RaycastHit hit)
-    {
-        if(hit.collider.TryGetComponent<ICanDead>(out ICanDead dead)
-            && _inventory[_selectItemIndex.Value].ItemType == ItemType.AED)
+        else if (hit.collider.TryGetComponent<ICanDead>(out ICanDead dead) &&
+            _inventory[_selectItemIndex.Value].ItemType == ItemType.AED)
         {
             _inventory[_selectItemIndex.Value].UseItem(hit.collider.gameObject);
+            return true;
         }
+        return false;
     }
 }
