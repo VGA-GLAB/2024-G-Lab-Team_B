85d6becc00be816b01ec89fcbe3807f458c6a435
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-04-28 14:30:09 +0900

    [add]アイテムの記録を行うクラスの追加

---
+++ Assets/Scripts/PlayerRecord/ItemPositionRecordLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>アイテムの座標の記録の読み込みをします</summary>
+public class ItemPositionRecordLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // データを読み込む
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 記録された時間と一致したら座標を更新する
+    }
+}
