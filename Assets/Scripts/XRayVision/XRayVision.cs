d10858fc101c64d443e500b0c974247055b6c83a
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-05-19 21:57:35 +0900

    [fix, add]エラーの修正、変数の修正。透視能力をプレハブ化

--- Assets/Scripts/XRayVision/XRayVision.cs
+++ Assets/Scripts/XRayVision/XRayVision.cs
@@ -9,54 +9,52 @@ public class XRayVision : MonoBehaviour
 {
     #region 変数
 
-    [Header("レイを飛ばす始点"), Tooltip("レイを飛ばす始点")]
-    [SerializeField] private Transform _rayStartPosition = default;
-
     [Header("レイの長さ"), Tooltip("レイの長さ")]
     [SerializeField] private float _maxRayDistance = 1000f;
 
-    [Header("能力がセットされているか"), Tooltip("能力がセットされているか")]
-    [SerializeField] private bool _setAvility = default; // set
-
-    [Header("透視するか"), Tooltip("透視するか")]
-    [SerializeField] private bool _isXRayVision = default; // use
-
     [Header("透過率(%)"), Tooltip("透過率(%)")]
-    [SerializeField, Range(0, 100)] private int _PercentageOfDitherLevel = 70;
-
-    [Header("現在のシェーダーのパラメーター"), Tooltip("現在のシェーダーのパラメーター")]    // 確認用
-    [SerializeField, Range(0, 16)] private float _ditherLevel = default;
-
-    [Header("透視する物のタグの名前：TransparentObject"), Tooltip("レイヤーの名前：TransparentObject")]
-    [SerializeField] private string _tagName = "TransparentObject";
-
-    [Header("透視するオブジェクト"), Tooltip("透視するオブジェクト")]
-    [SerializeField] private GameObject _currentTarget = default;
-
-
-    // 確認用
-    [Header("現在のマテリアル"), Tooltip("現在のマテリアル")]
-    [SerializeField] Material _currentMaterial = default;   // 現在のターゲット
-
-    [Header("透視していたオブジェクト"), Tooltip("透視していたオブジェクト")]
-    [SerializeField] GameObject _oldTarget = default;       // 古いターゲット
-
-    [Header("(instanse)が入ることはない"), Tooltip("元のマテリアル")]
-    [SerializeField] Material _defaultMaterial = default;   // 透視後元に戻す用1
-
-    [Header("(instanse)が入る)"), Tooltip("インスタンスされた古いターゲットのマテリアル")]
-    [SerializeField] Material _oldMaterial = default;       // 透視後削除する用
-
-    [Header("(instanse)が入ることはない"), Tooltip("古いターゲットの元のマテリアル")]
-    [SerializeField] Material _oldDefaultMaterial = default; // 透視後元に戻す用2
+    [SerializeField, Range(0, 100)] private int _percentageOfDitherLevel = 70;
 
     [Header("使用制限時間"), Tooltip("使用制限時間")]
     [SerializeField] private float _timeLimit = 40f;
 
+    [Tooltip("レイを飛ばす始点")]
+    private Transform _rayStartPosition = default;
+
+    [Tooltip("能力がセットされているか")]
+    private bool _setAvility = default; // set
+
+    [Tooltip("透視するか")]
+    private bool _isXRayVision = default; // use
+
+    [Tooltip("現在のシェーダーのパラメーター")]    // 0～16
+    private float _ditherLevel = default;
+
+    [Tooltip("タグの名前：TransparentObject")]
+    private string _tagName = "TransparentObject";
+
+    [Tooltip("透視するオブジェクト")]
+    private GameObject _currentTarget = default;    // 現在のターゲット
+
+    [Tooltip("透視していたオブジェクト")]
+    private GameObject _oldTarget = default;        // 古いターゲット
+
+    [Tooltip("現在のマテリアル")]
+    private Material _currentMaterial = default;
+
+    [Tooltip("元のマテリアル")]
+    private Material _defaultMaterial = default;    // 透視後元に戻す用1(instanse)が入ることはない
+
+    [Tooltip("古いターゲットの元のマテリアル")]
+    private Material _oldDefaultMaterial = default; // 透視後元に戻す用2(instanse)が入ることはない
+
+    [Tooltip("インスタンスされた古いターゲットのマテリアル")]
+    private Material _oldMaterial = default;        // 透視後削除する用(instanse)が入る)
+
 
     // テスト用
-    LineRenderer _lineRenderer = default;
-    Vector3 _rayHitPosition;
+    //private LineRenderer _lineRenderer = default;
+    //private Vector3 _rayHitPosition;
     #endregion
 
     /// <summary>
@@ -77,13 +75,16 @@ public class XRayVision : MonoBehaviour
         set => _isXRayVision = value;
     }
 
-    void Start()
+    private void Start()
     {
-        _ditherLevel = 16 * _PercentageOfDitherLevel * 0.01f;
-        _lineRenderer = GetComponent<LineRenderer>();
+        _ditherLevel = 16 * _percentageOfDitherLevel * 0.01f;
+        _rayStartPosition = Camera.main.transform;
+        
+        // テスト用：レイの可視化
+        //_lineRenderer = GetComponent<LineRenderer>();
     }
 
-    void Update()
+    private void Update()
     {
         RaycastGetObject(IsXRayVision);
 
@@ -92,7 +93,7 @@ public class XRayVision : MonoBehaviour
         {
             OnClick();
         }
-
+        
 
         // テスト用:能力選択(左クリック)
         //if (Input.GetButtonDown("Fire1"))
@@ -106,7 +107,7 @@ public class XRayVision : MonoBehaviour
     /// 呼ぶたびにフラグが切り替わる
     /// </summary>
     /// <param name="isFlag"></param>
-    public void OnClick()
+    private void OnClick()
     {
         IsXRayVision = !IsXRayVision;
         RaycastGetObject(IsXRayVision);
@@ -114,10 +115,10 @@ public class XRayVision : MonoBehaviour
 
     /// <summary>
     /// 能力を使用しているとき
-    /// "TransparentWall"タグのついた壁を透視する
+    /// "TransparentObject"タグのついた壁を透視する
     /// </summary>
     /// <param name="isFlag"></param>
-    void RaycastGetObject(bool isFlag)
+    private void RaycastGetObject(bool isFlag)
     {
         if (isFlag)
         {
@@ -125,8 +126,8 @@ public class XRayVision : MonoBehaviour
             Ray ray = new Ray(_rayStartPosition.position, this.transform.forward);
             RaycastHit hit;
 
-            // レイを前方に最大距離飛ばす
-            _rayHitPosition = _rayStartPosition.position + this.transform.forward * _maxRayDistance;
+            // テスト用：レイを前方に最大距離飛ばす
+            //_rayHitPosition = _rayStartPosition.position + this.transform.forward * _maxRayDistance;
 
             if (_currentTarget != null && _currentTarget.CompareTag(_tagName))
             {
@@ -157,14 +158,14 @@ public class XRayVision : MonoBehaviour
 
 
             // テスト用：レイが衝突したらそれ以上レイを伸ばさないようにする
-            if (Physics.Raycast(ray, out hit))
-            {
-                _rayHitPosition = hit.point;
-            }
+            //if (Physics.Raycast(ray, out hit))
+            //{
+            //    _rayHitPosition = hit.point;
+            //}
 
-            // テスト用：レイを可視化する
-            _lineRenderer.SetPosition(0, _rayStartPosition.position);
-            _lineRenderer.SetPosition(1, _rayHitPosition);
+            //// テスト用：レイを可視化する
+            //_lineRenderer.SetPosition(0, _rayStartPosition.position);
+            //_lineRenderer.SetPosition(1, _rayHitPosition);
         }
         else
         {
@@ -177,14 +178,13 @@ public class XRayVision : MonoBehaviour
             _currentTarget = null;
             _currentMaterial = null;
         }
-
     }
 
     /// <summary>
     /// 元のマテリアルに戻し、
     /// インスタンスされたマテリアルを破棄する
     /// </summary>
-    void DestroyMaterial()
+    private void DestroyMaterial()
     {
         if (_oldTarget != null)
         {
@@ -204,7 +204,7 @@ public class XRayVision : MonoBehaviour
     /// <summary>
     /// 元のマテリアルを保存しておく
     /// </summary>
-    void SaveMaterial(Material material)
+    private void SaveMaterial(Material material)
     {
         // 元のマテリアルが空だったらセットする
         if (_defaultMaterial != material)
@@ -219,7 +219,7 @@ public class XRayVision : MonoBehaviour
     /// <summary>
     /// 能力の使用制限時間
     /// </summary>
-    void CountDownTimer()
+    private void CountDownTimer()
     {
         _timeLimit -= Time.deltaTime;
         if (_timeLimit <= 0)
