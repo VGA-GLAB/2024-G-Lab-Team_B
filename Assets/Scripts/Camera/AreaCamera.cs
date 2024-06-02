e9374e25995e3731df26b77f6be75028ced2e4b9
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-06-02 15:09:59 +0900

    [update]エリアのカメラのスクリプト

--- Assets/Scripts/Camera/AreaCamera.cs
+++ Assets/Scripts/Camera/AreaCamera.cs
@@ -9,46 +9,91 @@ public class AreaCamera : MonoBehaviour
 {
     CameraSwitcher _cameraSwitcher;
 
+    
+
     [SerializeField, Header("一人称カメラ")]
-    private CinemachineVirtualCameraBase _inAreaFirstPerson;//一人称
+    private CinemachineVirtualCamera _inAreaFirstPerson;//一人称
 
     [SerializeField, Header("三人称カメラ")]
     private CinemachineFreeLook _inAreaThirdPerson;//三人称
 
-    private bool _isAreaCheng = false;//エリアが変わったかどうか
-    public bool IsAreaCheng { get => _isAreaCheng; set => _isAreaCheng = value; }
+    //CinemachineOrbitalTransposer _orbitalTransposer;
 
-    public CinemachineVirtualCameraBase InAreaFirstPerson { get => _inAreaFirstPerson; set => _inAreaFirstPerson = value; }
+    public CinemachineVirtualCamera InAreaFirstPerson { get => _inAreaFirstPerson; set => _inAreaFirstPerson = value; }
 
     public CinemachineFreeLook InAreaThirdPerson { get => _inAreaThirdPerson; set => _inAreaThirdPerson = value; }
+
+    [Header("どれくらい近くまでを描画するか")]
+    public float _nearClipDistance;
+
+    [Header("どれくらい遠くまでを描画するか")]
+    public float _farClipDistance;
+
+    [Header("優先するカメラの値（Priority）")]
+    public int _highPriority;
+
+    [Header("優先しないカメラの値（Priority）")]
+    public int _lowPriority;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cameraSwitcher = FindObjectOfType<CameraSwitcher>();
+        //_orbitalTransposer =FindObjectOfType<CinemachineOrbitalTransposer>();
     }
 
     
 
-    // Update is called once per frame
-    void Update()
+    public void Return()
     {
+        _inAreaFirstPerson.Priority = _lowPriority;
+        _inAreaThirdPerson.Priority = _lowPriority;
+    }
 
+    // Update is called once per frame
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //_inAreaFirstPerson.Priority = _highPriority;
+            if (_cameraSwitcher.IsFirstPerson == true)
+            {
+                _inAreaFirstPerson.Priority = _highPriority;
+
+            }
+            else
+            {
+                _inAreaThirdPerson.Priority = _highPriority;
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (Input.GetButtonDown("Fire2"))
+            {
+                if (_cameraSwitcher.IsFirstPerson == true)
+                {
+                    _inAreaFirstPerson.Priority = _lowPriority;
+                    _inAreaThirdPerson.Priority = _highPriority;
+                }
+                else
+                {
+                    _inAreaThirdPerson.Priority = _lowPriority;
+                    _inAreaFirstPerson.Priority = _highPriority;
 
-            _inAreaFirstPerson.Priority = 10;
-            //_inAreaThirdPerson.Priority = 10;
+                        _inAreaThirdPerson.ChangeToFreeLook(_inAreaFirstPerson);
+                }
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _inAreaFirstPerson.Priority = 0;
-        _inAreaThirdPerson.Priority = 0;
+        Return();
     }
 }
